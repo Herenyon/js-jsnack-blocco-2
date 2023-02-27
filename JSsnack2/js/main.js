5e24dc95d1c1baefd4f0ebca9c52ca255c93efe9
Herenyon <rikitur17@gmail.com>
2023-02-27 11:56:58 +0100

    first commit

---
+++ JSsnack2/js/main.js
@@ -0,0 +1,14 @@
+// Inserisci un numero, se è pari stampa il numero,
+//     se è dispari stampa il numero successivo
+
+let input = parseInt(prompt("Inserisci un numero"));
+
+if (input % 2 === 0){
+
+    console.log(input);
+}
+else {
+    input = input + 1;
+    console.log(input);
+
+}
