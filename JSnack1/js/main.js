03a0536fe1ff5b7b350e5a847a2676956649dfd1
Herenyon <rikitur17@gmail.com>
2023-03-01 11:04:24 +0100

    aggiunta consegna esercizio

--- JSnack1/js/main.js
+++ JSnack1/js/main.js
@@ -1,5 +1,10 @@
 'use strict'
 
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
 let somma = 0;
 
 for(let i = 0 ;i < 5; i++){
